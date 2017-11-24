d365d5885f4504dfdbcdef41dd967ddd919e411e
Adedayo-A <adesegunadedayo@hotmail.com>
2017-11-24 05:00:24 +0000

    changed file directory and creating server side in progress

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,35 +1,160 @@
 const centers = [
 	{
+		"id": "1",
 		"name": "Grace hall",
-		"capacity": "2000"
-		"rating": "5 star"
+		"capacity": "2000",
+		"rating": "* * * * *",
 		"Upcoming events": "Award ceremony",
-		"details": "Ikoyi ball"
+		"descriptions": "Beside the popular red ocean mall"
 	},
 
-	'Indian-potato': {
-		'name': 'Indian-potato',
-		'ingredients':["Black Chana with Potato", "turmeric", "tomato", "cucumber", "Kachumbar", "tamarind sauce fish curry"],
-		'upvotes': 28,
-		'downvotes': 6,
-		'preparation':"Black Chana with Potato. , {turmeric, and cumin in this classic, Tomato Cucumber Kachumbar, Red Split Lentils (Masoor Dal), coconut Chutney, Tamarind Sauce Fish Curry"
+	 {
+	 	"id": "2",
+		"name": "Ballerina hall",
+		"capacity": "2500",
+		"rating": "* * * * *",
+		"Upcoming events": "Wedding ceremony",
+		"descriptions":"20 mins drive from the mainland exit"
 	},	
 
 
-	'Salmon': {
-		'name': 'Salmon',
-		'ingredients':['salmon', 'lemon juice', 'gaelic', 'melted butter', 'salt', 'pepper', 'oregano', 'red pepper flakes'],
-		'upvotes': 20,
-		'downvotes': 7,
-		'preparation':"Preheat oven to 375 degrees F. Line a baking sheet with foil, In a small bowl, add lemon juice, garlic and melted butter, Place salmon onto prepared baking sheet, Pour the butter mixture over the salmon, Season with salt, pepper, oregano, and red pepper flakes, Fold the sides of the foil over the salmon"
+	{
+		"id": "3",
+		"name": "Friends hall",
+		"capacity": "1500",
+		"rating": "* * * * *",
+		"Upcoming events": "Music show",
+		"descriptions": "From the airport, take the lane 3 down"
 	},
 
 
-	'Sandwich': {
-		'name': 'Sandwich',
-		'ingredients':['salmon', 'mayonnaise', 'celery', 'lemon zest', 'black pepper', 'lettuce leaves', 'bread'],
-		'upvotes': 31,
-		'downvotes': 8,
-		'preparation': "Heat oven to 450° F. Roast the salmon fillet on a rimmed baking sheet until cooked through, 6 to 8 minutes; let cool, then flake, Combine the smoked salmon, mayonnaise, celery, chives, capers, lemon zest, flaked salmon, and ¼ teaspoon pepper in a medium bowl then divide the salmon salad and lettuce leaves among the bread."
+	{
+		"id": "4",
+		"name": "Diamond's hall",
+		"capacity": "2500",
+		"rating": "* * * * * *",
+		"Upcoming events": "Blue governor's conference",
+		"descriptions": "Behind the Government's square buildings"
 	}
 ]
+
+
+const events = [
+	{
+		"id": "event1",
+		"name of event": "race party",
+		"Date of event": "12/12/2017",
+		"location": "Diamond's hall",
+		"descriptions": "Behind the Government's square buildings",
+		"Host": "Fola Ajayi"
+		
+	},
+
+	 {
+		"id": "event2",
+		"name of event": "Entrepreneurship meet-up",
+		"Date of event": "15/12/2017",
+		"location": "Grace hall",
+		"descriptions": "Beside the popular red ocean mall",
+		"Host": "Mary swiss"
+	},
+
+
+	{
+		"id": "event3",
+		"name of event": "Merit Award ceremony",
+		"Date of event": "17/12/2017",
+		"location": "Friend's hall",
+		"descriptions": "From the airport, take the lane 3 down",
+		"Host": "Ankers",
+	},
+
+
+	{
+		"id": "event4",
+		"name of event": "Karaoke night",
+		"Date of event": "13/12/2017",
+		"location": "Ballerina hall",
+		"descriptions": "20 mins drive from the mainland exit",
+		"Host": "Paul Thomas"
+	}
+]
+
+
+
+ module.exports = (app) => {
+   app.get('/api', (req, res) => res.status(200).send({
+     message: 'Welcome to the BOOK Events API!',
+   }));
+ 
+   // list all centers
+   app.get('/api/centers', (req, res) => {
+		var recipesList = [];
+		// convert recipes object to list
+		Object.keys(recipes).forEach((key) => {
+			recipesList.push(recipes[key])
+		})
+
+		if (req.query.sort === 'upvotes') {
+			// sort recipes by ascending order of upvotes
+			recipesList.sort((a, b) => {
+				return a.upvotes - b.upvotes
+			});
+		}
+		res.status(200).send({
+			message: recipesList
+		});
+ 	})
+ 
+
+    // Add a center
+ 	app.post('/api/recipes', (req, res) => {
+ 		recipes[req.body.id] = req.body
+	   	res.status(200).send({
+	     	message: ['center added successfully', recipes]
+	   	});
+ 	})
+ 	// create an event
+ 	app.post('/api/recipes', (req, res) => {
+ 		recipes[req.body.name] = req.body
+	   	res.status(200).send({
+	     	message: ['event added successfully', recipes]
+	   	});
+ 	})
+
+ 	// get a center
+ 	app.get('/api/recipes/id', (req, res) => {
+	   	res.status(200).send({
+	     	message: recipes[req.params.recipeId]
+	   	});
+ 	})
+
+ 	// get an event
+ 	app.get('/api/recipes/id', (req, res) => {
+	   	res.status(200).send({
+	     	message: recipes[req.params.recipeId]
+	   	});
+ 	})
+ 
+ 	// Modify the details of center
+ 	app.put('/api/recipes/:id', (req, res) => {
+ 		recipes[req.params.recipeId] = req.body
+	   	res.status(200).send({
+	     	message: ['center updated successfully', recipes]
+	   	});
+ 	})
+
+ 	// Modify the details of events
+ 	app.put('/api/recipes/:id', (req, res) => {
+ 		recipes[req.params.recipeId] = req.body
+	   	res.status(200).send({
+	     	message: ['event updated successfully', recipes]
+	   	});
+ 	})
+
+ 	// delete an event
+ 	app.delete('/api/recipes/:id', (req, res) => {
+ 		delete recipes[req.params.recipeId]
+ 		res.status(200).send([{message:'recipe deleted successfully'}, recipes])
+ 	})
+ }
