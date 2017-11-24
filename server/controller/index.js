157114682d51f5da3ac1da6b14677e014cf1792e
Adedayo-A <adesegunadedayo@hotmail.com>
2017-11-24 00:21:34 +0000

    server-side of event manager

---
+++ server/controller/index.js
@@ -0,0 +1,35 @@
+const centers = [
+	{
+		"name": "Grace hall",
+		"capacity": "2000"
+		"rating": "5 star"
+		"Upcoming events": "Award ceremony",
+		"details": "Ikoyi ball"
+	},
+
+	'Indian-potato': {
+		'name': 'Indian-potato',
+		'ingredients':["Black Chana with Potato", "turmeric", "tomato", "cucumber", "Kachumbar", "tamarind sauce fish curry"],
+		'upvotes': 28,
+		'downvotes': 6,
+		'preparation':"Black Chana with Potato. , {turmeric, and cumin in this classic, Tomato Cucumber Kachumbar, Red Split Lentils (Masoor Dal), coconut Chutney, Tamarind Sauce Fish Curry"
+	},	
+
+
+	'Salmon': {
+		'name': 'Salmon',
+		'ingredients':['salmon', 'lemon juice', 'gaelic', 'melted butter', 'salt', 'pepper', 'oregano', 'red pepper flakes'],
+		'upvotes': 20,
+		'downvotes': 7,
+		'preparation':"Preheat oven to 375 degrees F. Line a baking sheet with foil, In a small bowl, add lemon juice, garlic and melted butter, Place salmon onto prepared baking sheet, Pour the butter mixture over the salmon, Season with salt, pepper, oregano, and red pepper flakes, Fold the sides of the foil over the salmon"
+	},
+
+
+	'Sandwich': {
+		'name': 'Sandwich',
+		'ingredients':['salmon', 'mayonnaise', 'celery', 'lemon zest', 'black pepper', 'lettuce leaves', 'bread'],
+		'upvotes': 31,
+		'downvotes': 8,
+		'preparation': "Heat oven to 450° F. Roast the salmon fillet on a rimmed baking sheet until cooked through, 6 to 8 minutes; let cool, then flake, Combine the smoked salmon, mayonnaise, celery, chives, capers, lemon zest, flaked salmon, and ¼ teaspoon pepper in a medium bowl then divide the salmon salad and lettuce leaves among the bread."
+	}
+]
